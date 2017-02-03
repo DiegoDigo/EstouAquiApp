611ddd186c31be530b64eaccc911618c002935bb
Diego <di3g0d0ming05@gmail.com>
2017-02-03 16:51:00 -0200

    Ajuste no layout

--- app/src/main/java/com/diego/estouaqui/LoginActivity.java
+++ app/src/main/java/com/diego/estouaqui/LoginActivity.java
@@ -1,13 +1,23 @@
 package com.diego.estouaqui;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView textApresentacao ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Typeface fontApresentacao = Typeface.createFromAsset(this.getAssets(),"Bairro-Regular.ttf");
+
+        textApresentacao = (TextView) findViewById(R.id.NameApp);
+        textApresentacao.setTypeface(fontApresentacao);
+
     }
 }
