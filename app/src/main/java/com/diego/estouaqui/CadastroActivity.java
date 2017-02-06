b8c2c69810a5c3dc6048a37fdd0797e609f0d829
Diego <di3g0d0ming05@gmail.com>
2017-02-06 16:01:04 -0200

    Tela de cadastro

--- app/src/main/java/com/diego/estouaqui/CadastroActivity.java
+++ app/src/main/java/com/diego/estouaqui/CadastroActivity.java
@@ -2,12 +2,38 @@ package com.diego.estouaqui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private ImageView buttonBack;
+    private Button buttonForgot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        buttonBack = (ImageView) findViewById(R.id.backButton);
+        buttonForgot = (Button) findViewById(R.id.forgot);
+
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        buttonForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Esqueceu a senha ?", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/diego/estouaqui/LoginActivity.java
+++ app/src/main/java/com/diego/estouaqui/LoginActivity.java
@@ -1,13 +1,17 @@
 package com.diego.estouaqui;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView textApresentacao ;
+    private Button createCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,19 @@ public class LoginActivity extends AppCompatActivity {
         Typeface fontApresentacao = Typeface.createFromAsset(this.getBaseContext().getAssets(),"Barrio-Regular.ttf");
 
         textApresentacao = (TextView) findViewById(R.id.NameApp);
+        createCount = (Button) findViewById(R.id.criarConta);
         textApresentacao.setTypeface(fontApresentacao);
+
+        createCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), CadastroActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
 
     }
 }
