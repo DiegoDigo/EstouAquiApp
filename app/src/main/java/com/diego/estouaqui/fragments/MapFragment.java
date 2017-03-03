a074e61bdd99205578189c8f12600d7ac30c8e5e
Diego <di3g0d0ming05@gmail.com>
2017-03-03 14:08:55 -0300

    Criaçao de fragments

--- app/src/main/java/com/diego/estouaqui/fragments/MapFragment.java
+++ app/src/main/java/com/diego/estouaqui/fragments/MapFragment.java
@@ -18,7 +18,7 @@ public class MapFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.map_layout, container, false);
+        return inflater.inflate(R.layout.activity_main, container, false);
     }
 }
 
--- app/src/main/java/com/diego/estouaqui/views/Home/HomeActivity.java
+++ app/src/main/java/com/diego/estouaqui/views/Home/HomeActivity.java
@@ -3,6 +3,7 @@ package com.diego.estouaqui.views.Home;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -19,15 +20,23 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setToolBar();
 
         mTab = (TabLayout) findViewById(R.id.tab_home);
         mViewPager = (ViewPager) findViewById(R.id.conteiner_home);
 
         mViewPager.setAdapter(new FragmentsAdapter(getSupportFragmentManager(), getResources().getStringArray(R.array.titulos)));
+        mTab.setTabGravity(TabLayout.GRAVITY_FILL);
         mTab.setupWithViewPager(mViewPager);
 
+
+    }
+
+    private void setToolBar() {
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(getResources().getString(R.string.app_name));
 
     }
 
